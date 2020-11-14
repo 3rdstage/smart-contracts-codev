84d9a782b49ef3228b5c8125e79dee7813754ee3
Sangmoon Oh <halfface@chollian.net>
2020-11-14 20:29:10 +0900

    Successfully finished to test for vote with 4 scenarios.

--- test/v2/TestVotes.js
+++ test/v2/TestVotes.js
@@ -25,20 +25,24 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
     return [chance, admin];
   }
   
-  async function getAndPrintBalances(title){
+  async function getAndPrintBalances(title, verbose = false){
     const bals = votees.concat(voters);
     bals.push({name: 'project manager', 
                addr: prjMgrContr.address, 
                amount: await tokenContr.balanceOf(prjMgrContr.address)});
 
     // query current balances
+    const nf = new Intl.NumberFormat('en')
     for(const bal of bals){
       bal.amount = await tokenContr.balanceOf(bal.addr);
+      bal.wei = nf.format(bal.amount.toString());
       bal.esv = web3.utils.fromWei(bal.amount);
     }    
 
-    console.debug(`\n[[ ${title} ]]`);
-    console.table(bals, ['name', 'addr', 'amount', 'esv']);
+    if(verbose){
+      console.debug(`\n[[ ${title} ]]`);
+      console.table(bals, ['name', 'addr', 'wei', 'esv']);
+    }
     
     return bals;
   }
@@ -60,35 +64,29 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
     //await getAndPrintBalances(`Initiall Token Balances`)
   });
   
-  it("Can vote", async() => {
-    
-    const bals0 = await getAndPrintBalances(`Token Balances before Vote`);
+  async function tryVoteAndVerify(prj, vteeAddrs, vts, vtTitle, verbose = false){
     
     const [chance, admin] = await prepareFixtures();
-    const rwdMdl = await prjMgrContr.getRewardModel(0);
-    
-    const epc = Date.now();
-    const prj = {
-      id: epc.toString().substring(3), 
-      name: 'Prj 1', 
-      totalReward: toBN(1E20),
-      totalRewardESV: web3.utils.fromWei(toBN(1E20)),
-      contribsPrct: 70,
-      rewardMdlAddr: rwdMdl.addr
+    if(verbose){
+      let hr = '#'.repeat(vtTitle.length + 12)
+      console.log(`${hr}\n###   ${vtTitle}   ###\n${hr}`);
     }
     
-    //const vtrs = [];
-    //for(const vtr of voters) vtrs.push(vtr.address);
+    const bals0 = await getAndPrintBalances(`Token Balances before Vote`, verbose);
+    
     // Create a new project
     await prjMgrContr.createProject(prj.id, prj.name, 
         prj.totalReward, prj.contribsPrct, prj.rewardMdlAddr, {from: admin});
     
-    console.debug("\n[[ Project Created ]]");
-    console.table(prj, ['Values']);
+    if(verbose){
+      prj.reward = new Intl.NumberFormat('en').format(web3.utils.fromWei(prj.totalReward));
+      const prjs = [prj];
+      console.debug("\n[[ Project Created ]]");
+      console.table(prjs, ['id', 'reward', 'contribsPrct']);
+    }
     
-    // Assign 3 voters to the project 
-    await prjMgrContr.assignProjectVoters(
-        prj.id, voters.map(e => e.addr), {from: admin});
+    // Assign voters to the project 
+    await prjMgrContr.assignProjectVoters(prj.id, vts.map(e => e.voterAddr), {from: admin});
     
     // Query voters of the project
     const vtrs = await prjMgrContr.getProjectVoters(prj.id);
@@ -97,43 +95,41 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
       vtrs2.push({project: prj.id, voter: voters.find(e => e.addr == vtrs[i]).name, voterAddr: vtrs[i]});         ;
     }
     
-    console.debug("\n[[ Voters Assigned ]]");
-    console.table(vtrs2);
+    if(verbose){
+      console.debug("\n[[ Voters Assigned ]]");
+      console.table(vtrs2);
+    }
     
     const cntrbs = [];
-    for(const i of [0, 1]){
+    for(const [i, addr] of vteeAddrs.entries()){
       cntrbs.push({project: prj.id, title: `Contrib ${i}`, 
-          owner: votees[i].name, ownerAddr: votees[i].addr});
+          owner: votees.find(e => e.addr == addr).name, ownerAddr: addr});
     }
-    //cntrbs.push({project: prj.id, ownerAddr: votees[0].addr, title: "Contrib 1"});
-    //cntrbs.push({project: prj.id, owner: votees[1].addr, title: "Contrib 2"});
     
-    // Add 2 contributions to the project
+    // Add contributions to the project
     for(const cntrb of cntrbs){
       await cntrbsContr.addOrUpdateContribution(
         toBN(cntrb.project), cntrb.ownerAddr, cntrb.title, {from: admin});
     }
     
-    console.log("\n[[ Contributions Registered ]]")
-    console.table(cntrbs);
+    if(verbose){
+      console.log("\n[[ Contributions Registered ]]")
+      console.table(cntrbs);
+    }
     
     // Create data to vote
-    const vts0 = []; // votes to cast
-    vts0.push({project: prj.id, voterAddr: voters[0].addr, 
-        voteeAddr: cntrbs[0].ownerAddr, amount: toBN(3E18)});
-    vts0.push({project: prj.id, voterAddr: voters[1].addr, 
-        voteeAddr: cntrbs[0].ownerAddr, amount: toBN(3E18)});
-    vts0.push({project: prj.id, voterAddr: voters[2].addr, 
-        voteeAddr: cntrbs[1].ownerAddr, amount: toBN(4E18)});
-    
+    const vts0 = vts; // votes to cast
     for(const vt0 of vts0){
+      vt0.project = prj.id;
       vt0.votee = votees.find(e => e.addr == vt0.voteeAddr).name;
       vt0.voter = voters.find(e => e.addr == vt0.voterAddr).name;
       vt0.esv = web3.utils.fromWei(vt0.amount);
     }
 
-    console.debug("\n[[ Votes to Cast (before cast) ]]")
-    console.table(vts0, ['project', 'voter', 'votee', 'esv']);
+    if(verbose){
+      console.debug("\n[[ Votes to Cast (before cast) ]]")
+      console.table(vts0, ['project', 'voter', 'votee', 'esv']);
+    }
     
     // Approve token and cast vote
     for(const vt of vts0){
@@ -155,7 +151,7 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
       let val = vts0.filter(e => e.voteeAddr == scr1.owner).reduce((acc, cur) => {return acc.add(cur.amount);}, toBN(0));
       assert.isTrue(toBN(scr1.value).eq(val));
     }
-    console.log('Votes are verified');
+    if(verbose) console.log('Votes are verified');
 
     // Display queried votes and scores    
     const vts2 = []; // queried votes
@@ -163,18 +159,23 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
       vts2.push({project: prj.id, voter: voters.find(e => e.addr == vt1.voter).name, 
           votee: votees.find(e => e.addr == vt1.votee).name, esv: web3.utils.fromWei(vt1.amount)});
     }
-    console.debug("\n[[ Votes Queried ]]");
-    console.table(vts2, ['project', 'voter', 'votee', 'esv']);
+    if(verbose){
+      console.debug("\n[[ Votes Queried ]]");
+      console.table(vts2, ['project', 'voter', 'votee', 'esv']);
+    }
+    
     const scrs2 = []
     for(const scr1 of scrs1){
       scrs2.push({project: prj.id, votee: votees.find(e => e.addr == scr1.owner).name,
           esv: web3.utils.fromWei(scr1.value)});
     }
-    console.debug("\n[[ Scores Queried ]]");
-    console.table(scrs2);
+    if(verbose){
+      console.debug("\n[[ Scores Queried ]]");
+      console.table(scrs2);
+    }
     
     // Get balances after votes
-    const bals1 = await getAndPrintBalances('Balances after Vote');
+    const bals1 = await getAndPrintBalances('Balances after Vote', verbose);
     
     // Verfiy balances
     for(const bal1 of bals1){
@@ -192,9 +193,108 @@ contract("'Votes' contract uint tests suite 1", async accounts => {
         assert.isTrue(bal1.amount.eq(amt.add(sm)));
       }
     }
-    console.log('Balances are verified');
+    if(verbose) console.log('Balances are verified');
+  }
+  
+  
+  it("Can vote according to the contest scenario.", async() => {
     
-
+    const [chance, admin] = await prepareFixtures();
+    const rwdMdl = await prjMgrContr.getRewardModel(0);
+    
+    const epc = Date.now();
+    const prj = {
+      id: epc.toString().substring(3), 
+      name: 'Prj 1', 
+      totalReward: toBN(1E20),
+      totalRewardESV: web3.utils.fromWei(toBN(1E20)),
+      contribsPrct: 70,
+      rewardMdlAddr: rwdMdl.addr
+    }
+    
+    const vteeAddrs = [ votees[0].addr, votees[1].addr ];
+    const vts = [];
+    vts.push({voterAddr: voters[0].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[1].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[2].addr, voteeAddr: vteeAddrs[1], amount: toBN(4E18)});
+ 
+    await tryVoteAndVerify(prj, vteeAddrs, vts, "Contest Scenario", true); 
+        
   });
+  
+  it("Can vote according to scenario 2", async() => {
+    
+    const [chance, admin] = await prepareFixtures();
+    const rwdMdl = await prjMgrContr.getRewardModel(0);
+    
+    const epc = Date.now();
+    const prj = {
+      id: epc.toString().substring(3), 
+      name: 'Prj 2', 
+      totalReward: toBN(1E20),
+      totalRewardESV: web3.utils.fromWei(toBN(1E20)),
+      contribsPrct: 70,
+      rewardMdlAddr: rwdMdl.addr
+    }
+    
+    const vteeAddrs = [ votees[0].addr, votees[1].addr ];
+    const vts = [];
+    vts.push({voterAddr: voters[0].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[1].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[2].addr, voteeAddr: vteeAddrs[1], amount: toBN(6E18)});
+ 
+    await tryVoteAndVerify(prj, vteeAddrs, vts, "Scenario 2", true); 
+        
+  });
+  
+  it("Can vote according to scenario 3", async() => {
+    
+    const [chance, admin] = await prepareFixtures();
+    const rwdMdl = await prjMgrContr.getRewardModel(0);
+    
+    const epc = Date.now();
+    const prj = {
+      id: epc.toString().substring(3), 
+      name: 'Prj 3', 
+      totalReward: toBN(1E20),
+      totalRewardESV: web3.utils.fromWei(toBN(1E20)),
+      contribsPrct: 70,
+      rewardMdlAddr: rwdMdl.addr
+    }
+    
+    const vteeAddrs = [ votees[0].addr, votees[1].addr ];
+    const vts = [];
+    vts.push({voterAddr: voters[0].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[1].addr, voteeAddr: vteeAddrs[0], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[2].addr, voteeAddr: vteeAddrs[0], amount: toBN(4E18)});
+ 
+    await tryVoteAndVerify(prj, vteeAddrs, vts, "Scenario 3", true); 
+        
+  });  
+  
+  it("Can vote according to scenario 4", async() => {
+    
+    const [chance, admin] = await prepareFixtures();
+    const rwdMdl = await prjMgrContr.getRewardModel(0);
+    
+    const epc = Date.now();
+    const prj = {
+      id: epc.toString().substring(3), 
+      name: 'Prj 4', 
+      totalReward: toBN(1E20),
+      totalRewardESV: web3.utils.fromWei(toBN(1E20)),
+      contribsPrct: 70,
+      rewardMdlAddr: rwdMdl.addr
+    }
+    
+    const vteeAddrs = [ votees[0].addr, votees[1].addr, votees[2].addr ];
+    const vts = [];
+    vts.push({voterAddr: voters[0].addr, voteeAddr: vteeAddrs[1], amount: toBN(7E18)});
+    vts.push({voterAddr: voters[1].addr, voteeAddr: vteeAddrs[2], amount: toBN(3E18)});
+    vts.push({voterAddr: voters[2].addr, voteeAddr: vteeAddrs[0], amount: toBN(4E18)});
+ 
+    await tryVoteAndVerify(prj, vteeAddrs, vts, "Scenario 4", true); 
+        
+  });    
  
 });
